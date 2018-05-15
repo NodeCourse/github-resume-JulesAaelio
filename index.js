5fba61ab3b62edf935f9a8fb982f3b49b122667d
Jules LAURENT <jules.laurent@ynov.com>
2018-05-15 11:08:52 +0200

    resume page ok

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const request = require('request-promise-native');
+const Request = request.defaults({
+        json: true,
+        headers: {
+            'User-Agent':"JulesAaelio"
+        }
+    });
 const app = new express();
 app.set('view engine', 'pug');
 app.use(express.static('public'));
@@ -11,6 +18,20 @@ app.use(bodyParser.urlencoded({
 console.log("App started at " , new Date().toLocaleString());
 app.get('/',(req,res) => {
     res.render('form');
+});
+
+app.get('/resume',(req,res) => {
+    console.log(req.query.username);
+   if(req.query.username) {
+        Request.get({
+            url: 'https://api.github.com/users/'+req.query.username
+        }).then(r => {
+            console.log(r);
+            res.render('resume',{
+                user: r
+            });
+        })
+   }
 });
 
 
