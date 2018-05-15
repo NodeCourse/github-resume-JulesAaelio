3c4e71ba02ee4412ff824e868a9dc4a40ebc34dd
Jules LAURENT <jules.laurent@ynov.com>
2018-05-15 10:44:26 +0200

    form page

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('Working');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = new express();
+app.set('view engine', 'pug');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+
+console.log("App started at " , new Date().toLocaleString());
+app.get('/',(req,res) => {
+    res.render('form');
+});
+
+
+app.listen(3000);
